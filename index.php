d31c303553125998e1c0c51d328e5501b04d1b29
naarika <naarika@rambler.ru>
2014-07-23 18:50:02 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title> title </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+</head>
+
+
+<body>
+
+<FORM  METHOD=POST action="Chekway.php">
+    <INPUT NAME="leter" TYPE="text" VALUE="e" placeholder="e">
+    <INPUT NAME="number" TYPE="text" VALUE="4" placeholder="4">
+    <INPUT NAME="submit" TYPE="submit" VALUE="ход">
+    <br><br>
+</FORM>
+
+
+
+</body>
+</html>
